5ed545319c5b40e85c2b7b341e1cf642219587cc
Awanish Khanal <awanishkhanal20@gmail.com>
2022-08-18 11:34:04 +0545

    Initial commit

---
+++ src/main/java/com/example/RestaurantMS/RestaurantMSApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestaurantMS/RestaurantMSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
